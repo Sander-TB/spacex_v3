0021e87d06ee5940b237ef98e6af6fd881d64348
Sander Byrkjeland <sunbyr@gmail.com>
2021-06-02 15:36:04 +0200

    done ish, still not that happy

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -37,33 +37,38 @@ export default function Rockets() {
 	if (error) return <div>An error has occurred, please reload the page</div>;
 
 	return (
-		<section>
-		<h2>Rockets</h2>
-			{rocketsInfo.map((rocket) => {
-				return (
-					<div key={rocket.id}>
-						<h3>{rocket.rocket_name}</h3>
-						<img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
-						<img src={rocket.flickr_images[1]} alt={rocket.rocket_name} />
-						<p>{rocket.description}</p>
-						<ul>
-							<li>Diameter: {rocket.diameter.meters} m</li>
-							<li>Mass: {rocket.mass.kg} kg</li>
-							<li>Height: {rocket.height.meters} m</li>
-							<li>First Flight: {rocket.first_flight}</li>
-							<li>Success Rate: {rocket.success_rate_pct}%</li>
-							<li>Cost Per Launch: ${rocket.cost_per_launch}</li>
-						</ul>
-						<a
-							href={rocket.wikipedia}
-							target='_blank'
-							rel='noopener noreferrer'
-							className='btn'>
-							Read More
-						</a>
-					</div>
-				);
-			})}
+		<section className="rockets">
+			<h2>Rockets</h2>
+				{rocketsInfo.map((rocket) => {
+					return (
+						<div key={rocket.id} className="rockets-card">
+							<h3>{rocket.rocket_name}</h3>
+							<div className="rockets-card-inner">		
+								<div className="inner-left">
+									<p>{rocket.description}</p>
+									<ul>
+										<li>Diameter: {rocket.diameter.meters} m</li>
+										<li>Mass: {rocket.mass.kg} kg</li>
+										<li>Height: {rocket.height.meters} m</li>
+										<li>First Flight: {rocket.first_flight}</li>
+										<li>Success Rate: {rocket.success_rate_pct}%</li>
+										<li>Cost Per Launch: ${rocket.cost_per_launch}</li>
+									</ul>
+									<a
+										href={rocket.wikipedia}
+										target='_blank'
+										rel='noopener noreferrer'
+										className='btn'>
+										Read More
+									</a>
+								</div>
+								<div className="inner-right">
+									<img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+								</div>	
+							</div>
+						</div>
+					)
+				})}
 		</section>
 	);
 }
